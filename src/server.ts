d6864956f1bb5864e06fb099925ed41977e5036a
swagenough <151718487+swagenough@users.noreply.github.com>
2025-01-11 00:35:49 +0700

    error handling

--- src/server.ts
+++ src/server.ts
@@ -12,15 +12,28 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-    console.log('welcome to express')
-    res.status(200)
-    res.json({message: 'wassup guys'})
+app.get('/', (req, res, next) => {
+    // console.log('welcome to express')
+    // res.status(200)
+    // res.json({message: 'wassup guys'})
+    setTimeout(() => {
+        next(new Error('hello'))
+    }, 1)
 })
 
 app.use('/api', protect, router)
 
 app.post('/user', createNewUser)
 app.post('/signin', signin)
+
+app.use((err, req, res, next) => {
+    if (err.type === 'auth') {
+        res.status(400).json({message: 'auth error'})
+    } else if (err.type === 'input') {
+        res.status(400).json({message: 'invalid input'})
+    } else {
+        res.status(500).json({message: 'in server handler'})
+    }
+})
 
 export default app
