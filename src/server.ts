27c5787a6e4c1aa3c10eab56b1100ca30a6e694c
swagenough <151718487+swagenough@users.noreply.github.com>
2025-01-15 17:25:49 +0700

    deploy + added some testing

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res, next) => {
-    setTimeout(() => {
-        next(new Error('hello'))
-    }, 1)
+    res.json({message: 'hello'})
 })
 
 app.use('/api', protect, router)
